9ab8adaea08b3b756ac3aa38d0ccb3e3ee4185df
HaniLeb <hani.lebguiret@epsi.fr>
2023-08-02 14:59:55 +0200

    Refacto methodes de test 5 et 8

--- NombresRomains.Test/NombresRomainsTest.cs
+++ NombresRomains.Test/NombresRomainsTest.cs
@@ -3,8 +3,8 @@ namespace NombresRomains.Test
     public class NombresRomainsTest
     {
         [Theory(DisplayName = "Etant donné le chiffre de 1 à 3" +
-                            "Quand je convertis chiffreArabe en NombreRomain" +
-                            "Alors j'obtiens I x fois")]
+                            " Quand je convertis chiffreArabe en NombreRomain" +
+                            " Alors j'obtiens I x fois")]
         [InlineData(1)]
         [InlineData(2)]
         [InlineData(3)]
@@ -18,8 +18,8 @@ namespace NombresRomains.Test
         }
 
         [Fact(DisplayName = "Etant donné le chiffre 4" +
-                            "Quand je convertis chiffreArabe en NombreRomain" +
-                            "Alors j'obtiens IV")]
+                            " Quand je convertis chiffreArabe en NombreRomain" +
+                            " Alors j'obtiens IV")]
         public void TestQuatre()
         {
             const uint chiffreArabe = 4;
@@ -30,55 +30,20 @@ namespace NombresRomains.Test
 
         }        
         
-        [Fact(DisplayName = "Etant donné le chiffre 5" +
-                            "Quand je convertis chiffreArabe en NombreRomain" +
-                            "Alors j'obtiens V")]
-        public void TestCinq()
+        [Theory(DisplayName = "Etant donné le chiffre de 5 à 8" +
+                            " Quand je convertis chiffreArabe en NombreRomain" +
+                            " Alors j'obtiens V plus x fois I")]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        public void TestCinq(uint chiffreArabe)
         {
-            const uint chiffreArabe = 5;
-
             var nombreRomain = Convertisseur.Convertir(chiffreArabe);
 
-            Assert.Equal("V", nombreRomain);
+            var iResult = new string('I', (int)chiffreArabe - 5);
 
-        }        
-        
-        [Fact(DisplayName = "Etant donné le chiffre 6" +
-                            "Quand je convertis chiffreArabe en NombreRomain" +
-                            "Alors j'obtiens 6")]
-        public void TestSix()
-        {
-            const uint chiffreArabe = 6;
-
-            var nombreRomain = Convertisseur.Convertir(chiffreArabe);
-
-            Assert.Equal("VI", nombreRomain);
-
-        }        
-        
-        [Fact(DisplayName = "Etant donné le chiffre 7" +
-                            "Quand je convertis chiffreArabe en NombreRomain" +
-                            "Alors j'obtiens 7")]
-        public void TestSept()
-        {
-            const uint chiffreArabe = 7;
-
-            var nombreRomain = Convertisseur.Convertir(chiffreArabe);
-
-            Assert.Equal("VII", nombreRomain);
-
-        }        
-        
-        [Fact(DisplayName = "Etant donné le chiffre 8" +
-                            "Quand je convertis chiffreArabe en NombreRomain" +
-                            "Alors j'obtiens 8")]
-        public void TestHuit()
-        {
-            const uint chiffreArabe = 8;
-
-            var nombreRomain = Convertisseur.Convertir(chiffreArabe);
-
-            Assert.Equal("VIII", nombreRomain);
+            Assert.Equal("V" + iResult, nombreRomain);
 
         }        
         
