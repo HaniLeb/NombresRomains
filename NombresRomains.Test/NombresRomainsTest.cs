6ebbb432f75b4ed86a6e95d515d825b3b32c546a
HaniLeb <hani.lebguiret@epsi.fr>
2023-08-02 14:13:11 +0200

    Deuxième test au rouge

--- NombresRomains.Test/NombresRomainsTest.cs
+++ NombresRomains.Test/NombresRomainsTest.cs
@@ -13,5 +13,18 @@ namespace NombresRomains.Test
 
             Assert.Equal("I", nombreRomain);
         }
+
+        [Fact(DisplayName = "Etant donné le chiffre 2" +
+                            "Quand je convertis chiffreArabe en NombreRomain" +
+                            "Alors j'obtiens II")]
+        public void Test2()
+        {
+            const uint chiffreArabe = 2;
+
+            var nombreRomain = Convertisseur.Convertir(chiffreArabe);
+
+            Assert.Equal("II", nombreRomain);
+
+        }
     }
 }
