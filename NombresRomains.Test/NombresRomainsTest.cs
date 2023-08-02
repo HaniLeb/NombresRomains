2e2a7971c77cdecdd040e8dd2819abe186d59fbf
HaniLeb <hani.lebguiret@epsi.fr>
2023-08-02 14:10:41 +0200

    Premier test refacto

--- NombresRomains.Test/NombresRomainsTest.cs
+++ NombresRomains.Test/NombresRomainsTest.cs
@@ -2,17 +2,16 @@ namespace NombresRomains.Test
 {
     public class NombresRomainsTest
     {
-        [Fact]
+        [Fact(DisplayName = "Etant donné le chiffre 1" +
+                            "Quand je convertis chiffreArabe en NombreRomain" +
+                            "Alors j'obtiens I")]
         public void Test1()
         {
-            // Etant donné le chiffre 1
             const uint chiffreArabe = 1;
 
-            // Quand je convertis chiffreArabe en NombreRomain
             var nombreRomain = Convertisseur.Convertir(chiffreArabe);
 
-            // Alors j'obtiens I
-            Assert.Equal("I", nombreRomain);    
+            Assert.Equal("I", nombreRomain);
         }
     }
 }
